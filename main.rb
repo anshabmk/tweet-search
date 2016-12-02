82e14c3fe8bc8398f717adcfb82f4ffdc3eb0d27
Anshab M K <anshab.mk@geckolyst.com>
2016-12-02 17:46:04 +0530

    Redis added

--- main.rb
+++ main.rb
@@ -1,12 +1,16 @@
 require 'sinatra'
 require 'twitter'
 require 'active_support/all'
+require 'json'
+#require 'redis'
+
+#redis = Redis.new
 
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key = "mhCWQ6xD08VXMIylJIZ98AXxh"
-  config.consumer_secret = "aawJm8w2XLrbHQO6SxYgKY4ii7ElNH97i4zGDx7V3IrCrQvAIR"
-  config.access_token = "800644619465670656-P832RHheNPDqT9zECCQyn6ZQPIYjwLa"
-  config.access_token_secret = "mrLqeqVgiziDK3P0aQPI5euBkvxkS8ANGflN1OSp2SlGs"
+	config.consumer_key = "mhCWQ6xD08VXMIylJIZ98AXxh"
+	config.consumer_secret = "aawJm8w2XLrbHQO6SxYgKY4ii7ElNH97i4zGDx7V3IrCrQvAIR"
+	config.access_token = "800644619465670656-P832RHheNPDqT9zECCQyn6ZQPIYjwLa"
+	config.access_token_secret = "mrLqeqVgiziDK3P0aQPI5euBkvxkS8ANGflN1OSp2SlGs"
 end
 
 get '/' do
@@ -14,23 +18,29 @@ get '/' do
 end
 
 post '/' do
-  @@retweets = []
-  @@tweets = client.search(params[:keyword], :result_type => "recent").take(100).collect
+  retweets = []
+  tweets = client.search(params[:keyword], :result_type => "recent").take(100).collect
 
-  @@tweets.each do |tweet|
-    @@retweets.push tweet if tweet.text.start_with? "RT"
+	tweets_hash = tweets.map { |tweet| { time: tweet.created_at, name: tweet.user.screen_name, text: tweet.text } }
+
+#	redis.set("#{params[:keyword]}",tweets_hash.to_json)
+
+  tweets.each do |tweet|
+    retweets.push tweet if tweet.text.start_with? "RT"
   end
 
-  erb :dashboard
+  erb :dashboard, :locals => { :tweets_hash => tweets,:retweets => retweets }
 end
 
-get '/results' do
-  erb :results
+post '/results' do
+	tweets = JSON.parse params[:tweets]
+
+	erb :results, :locals => { :tweets => tweets }
 end
 
 post '/results/filter' do
   filtered_tweets = []
-  
+
   case params[:filter_option]
   when "retweets"
     @@tweets.each do |tweet|
@@ -61,6 +71,6 @@ post '/results/filter' do
       filtered_tweets.push tweet if tweet.created_at.to_date <= (Date.today - 5.days)
     end
   end
-  
+
   erb :filter, :locals => {:filtered_tweets => filtered_tweets}
 end
